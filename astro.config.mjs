df5904eaf90a1f2b43f201b16f12695857c1b537
DantNg <nguyendatmc581@gmail.com>
2026-06-13 12:16:51 +0700

    modify astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://yourusername.github.io',
+  site: 'https://dantng.github.io/myportfolio',
   base: '/',
   integrations: [
     tailwind(),
