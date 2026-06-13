288f9d4e9a5aa4a1668ebfc7af150b90166e2c97
DantNg <nguyendatmc581@gmail.com>
2026-06-13 12:25:55 +0700

    fix github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://dantng.github.io',
-  base: '/MyPortfolio/',
+  base: '/MyPortfolio',
   integrations: [
     tailwind(),
-    ...(process.env.SITE ? [sitemap()] : []),
   ],
   markdown: {
     shikiConfig: {
